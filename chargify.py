d2296a5fb309756f1cc6a1e3884e3b7eb263aa22
Justin Clark <jpclark6@gmail.com>
2019-10-31 08:57:46 -0600

    added failsafe for edge case if data = None

--- chargify.py
+++ chargify.py
@@ -176,5 +176,6 @@ class Chargify(object):
 
     def __call__(self, **kwargs):
         url, method, data = self.construct_request(**kwargs)
-        data = data.encode('utf-8')
+        if data:
+            data = data.encode('utf-8')
         return self.client.make_request(url, method, data, self.api_key)
